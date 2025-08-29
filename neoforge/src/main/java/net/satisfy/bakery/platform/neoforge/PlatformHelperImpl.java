9cebc089dfccb92bfe2c2e8a518e60d7f1dc3988
satisfy <104036234+satisfyu@users.noreply.github.com>
2025-08-29 19:35:43 +0200

    fix(Config): Neoforge config

--- neoforge/src/main/java/net/satisfy/bakery/platform/neoforge/PlatformHelperImpl.java
+++ neoforge/src/main/java/net/satisfy/bakery/platform/neoforge/PlatformHelperImpl.java
@@ -4,275 +4,300 @@ import net.satisfy.bakery.neoforge.core.config.BakeryNeoForgeConfig;
 import net.satisfy.bakery.platform.PlatformHelper;
 
 public class PlatformHelperImpl extends PlatformHelper {
+
+    // Banner / Tooltip
     public static boolean shouldGiveEffect() {
-        return BakeryNeoForgeConfig.GIVE_EFFECT.get();
+        return BakeryNeoForgeConfig.give_effect;
     }
 
     public static boolean shouldShowTooltip() {
-        return BakeryNeoForgeConfig.GIVE_EFFECT.get() && BakeryNeoForgeConfig.SHOW_TOOLTIP.get();
+        return BakeryNeoForgeConfig.give_effect && BakeryNeoForgeConfig.show_tooltip;
     }
 
+    // Dough
+    public static int getCakeDoughNutrition() {
+        return BakeryNeoForgeConfig.cake_dough_nutrition;
+    }
+
+    public static float getCakeDoughSaturation() {
+        return (float) BakeryNeoForgeConfig.cake_dough_saturation;
+    }
+
+    public static int getSweetDoughNutrition() {
+        return BakeryNeoForgeConfig.sweet_dough_nutrition;
+    }
+
+    public static float getSweetDoughSaturation() {
+        return (float) BakeryNeoForgeConfig.sweet_dough_saturation;
+    }
+
+    // Breads
     public static int getCroissantNutrition() {
-        return BakeryNeoForgeConfig.CROISSANT_NUTRITION.get();
+        return BakeryNeoForgeConfig.croissant_nutrition;
     }
 
     public static float getCroissantSaturation() {
-        return BakeryNeoForgeConfig.CROISSANT_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.croissant_saturation;
     }
 
     public static int getCrustyBreadNutrition() {
-        return BakeryNeoForgeConfig.CRUSTY_BREAD_NUTRITION.get();
+        return BakeryNeoForgeConfig.crusty_bread_nutrition;
     }
 
     public static float getCrustyBreadSaturation() {
-        return BakeryNeoForgeConfig.CRUSTY_BREAD_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.crusty_bread_saturation;
     }
 
     public static int getBreadNutrition() {
-        return BakeryNeoForgeConfig.BREAD_NUTRITION.get();
+        return BakeryNeoForgeConfig.bread_nutrition;
     }
 
     public static float getBreadSaturation() {
-        return BakeryNeoForgeConfig.BREAD_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.bread_saturation;
     }
 
     public static int getBaguetteNutrition() {
-        return BakeryNeoForgeConfig.BAGUETTE_NUTRITION.get();
+        return BakeryNeoForgeConfig.baguette_nutrition;
     }
 
     public static float getBaguetteSaturation() {
-        return BakeryNeoForgeConfig.BAGUETTE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.baguette_saturation;
     }
 
     public static int getToastNutrition() {
-        return BakeryNeoForgeConfig.TOAST_NUTRITION.get();
+        return BakeryNeoForgeConfig.toast_nutrition;
     }
 
     public static float getToastSaturation() {
-        return BakeryNeoForgeConfig.TOAST_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.toast_saturation;
     }
 
     public static int getBraidedBreadNutrition() {
-        return BakeryNeoForgeConfig.BRAIDED_BREAD_NUTRITION.get();
+        return BakeryNeoForgeConfig.braided_bread_nutrition;
     }
 
     public static float getBraidedBreadSaturation() {
-        return BakeryNeoForgeConfig.BRAIDED_BREAD_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.braided_bread_saturation;
     }
 
+    // Sandwiches
     public static int getSandwichNutrition() {
-        return BakeryNeoForgeConfig.SANDWICH_NUTRITION.get();
+        return BakeryNeoForgeConfig.sandwich_nutrition;
     }
 
     public static float getSandwichSaturation() {
-        return BakeryNeoForgeConfig.SANDWICH_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.sandwich_saturation;
     }
 
     public static int getVegetableSandwichNutrition() {
-        return BakeryNeoForgeConfig.VEGETABLE_SANDWICH_NUTRITION.get();
+        return BakeryNeoForgeConfig.vegetable_sandwich_nutrition;
     }
 
     public static float getVegetableSandwichSaturation() {
-        return BakeryNeoForgeConfig.VEGETABLE_SANDWICH_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.vegetable_sandwich_saturation;
     }
 
     public static int getGrilledSalmonSandwichNutrition() {
-        return BakeryNeoForgeConfig.GRILLED_SALMON_SANDWICH_NUTRITION.get();
+        return BakeryNeoForgeConfig.grilled_salmon_sandwich_nutrition;
     }
 
     public static float getGrilledSalmonSandwichSaturation() {
-        return BakeryNeoForgeConfig.GRILLED_SALMON_SANDWICH_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.grilled_salmon_sandwich_saturation;
     }
 
     public static int getGrilledBaconSandwichNutrition() {
-        return BakeryNeoForgeConfig.GRILLED_BACON_SANDWICH_NUTRITION.get();
+        return BakeryNeoForgeConfig.grilled_bacon_sandwich_nutrition;
     }
 
     public static float getGrilledBaconSandwichSaturation() {
-        return BakeryNeoForgeConfig.GRILLED_BACON_SANDWICH_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.grilled_bacon_sandwich_saturation;
     }
 
     public static int getBreadWithJamNutrition() {
-        return BakeryNeoForgeConfig.BREAD_WITH_JAM_NUTRITION.get();
+        return BakeryNeoForgeConfig.bread_with_jam_nutrition;
     }
 
     public static float getBreadWithJamSaturation() {
-        return BakeryNeoForgeConfig.BREAD_WITH_JAM_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.bread_with_jam_saturation;
     }
 
+    // Cakes & Tarts (Slices)
     public static int getStrawberryCakeSliceNutrition() {
-        return BakeryNeoForgeConfig.STRAWBERRY_CAKE_SLICE_NUTRITION.get();
+        return BakeryNeoForgeConfig.strawberry_cake_slice_nutrition;
     }
 
     public static float getStrawberryCakeSliceSaturation() {
-        return BakeryNeoForgeConfig.STRAWBERRY_CAKE_SLICE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.strawberry_cake_slice_saturation;
     }
 
     public static int getSweetberryCakeSliceNutrition() {
-        return BakeryNeoForgeConfig.SWEETBERRY_CAKE_SLICE_NUTRITION.get();
+        return BakeryNeoForgeConfig.sweetberry_cake_slice_nutrition;
     }
 
     public static float getSweetberryCakeSliceSaturation() {
-        return BakeryNeoForgeConfig.SWEETBERRY_CAKE_SLICE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.sweetberry_cake_slice_saturation;
     }
 
     public static int getChocolateCakeSliceNutrition() {
-        return BakeryNeoForgeConfig.CHOCOLATE_CAKE_SLICE_NUTRITION.get();
+        return BakeryNeoForgeConfig.chocolate_cake_slice_nutrition;
     }
 
     public static float getChocolateCakeSliceSaturation() {
-        return BakeryNeoForgeConfig.CHOCOLATE_CAKE_SLICE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.chocolate_cake_slice_saturation;
     }
 
     public static int getChocolateGateauSliceNutrition() {
-        return BakeryNeoForgeConfig.CHOCOLATE_GATEAU_SLICE_NUTRITION.get();
+        return BakeryNeoForgeConfig.chocolate_gateau_slice_nutrition;
     }
 
     public static float getChocolateGateauSliceSaturation() {
-        return BakeryNeoForgeConfig.CHOCOLATE_GATEAU_SLICE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.chocolate_gateau_slice_saturation;
     }
 
     public static int getBundtCakeSliceNutrition() {
-        return BakeryNeoForgeConfig.BUNDT_CAKE_SLICE_NUTRITION.get();
+        return BakeryNeoForgeConfig.bundt_cake_slice_nutrition;
     }
 
     public static float getBundtCakeSliceSaturation() {
-        return BakeryNeoForgeConfig.BUNDT_CAKE_SLICE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.bundt_cake_slice_saturation;
     }
 
     public static int getLinzerTartSliceNutrition() {
-        return BakeryNeoForgeConfig.LINZER_TART_SLICE_NUTRITION.get();
+        return BakeryNeoForgeConfig.linzer_tart_slice_nutrition;
     }
 
     public static float getLinzerTartSliceSaturation() {
-        return BakeryNeoForgeConfig.LINZER_TART_SLICE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.linzer_tart_slice_saturation;
     }
 
     public static int getApplePieSliceNutrition() {
-        return BakeryNeoForgeConfig.APPLE_PIE_SLICE_NUTRITION.get();
+        return BakeryNeoForgeConfig.apple_pie_slice_nutrition;
     }
 
     public static float getApplePieSliceSaturation() {
-        return BakeryNeoForgeConfig.APPLE_PIE_SLICE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.apple_pie_slice_saturation;
     }
 
     public static int getGlowberryPieSliceNutrition() {
-        return BakeryNeoForgeConfig.GLOWBERRY_PIE_SLICE_NUTRITION.get();
+        return BakeryNeoForgeConfig.glowberry_pie_slice_nutrition;
     }
 
     public static float getGlowberryPieSliceSaturation() {
-        return BakeryNeoForgeConfig.GLOWBERRY_PIE_SLICE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.glowberry_pie_slice_saturation;
     }
 
     public static int getChocolateTartSliceNutrition() {
-        return BakeryNeoForgeConfig.CHOCOLATE_TART_SLICE_NUTRITION.get();
+        return BakeryNeoForgeConfig.chocolate_tart_slice_nutrition;
     }
 
     public static float getChocolateTartSliceSaturation() {
-        return BakeryNeoForgeConfig.CHOCOLATE_TART_SLICE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.chocolate_tart_slice_saturation;
     }
 
     public static int getPuddingSliceNutrition() {
-        return BakeryNeoForgeConfig.PUDDING_SLICE_NUTRITION.get();
+        return BakeryNeoForgeConfig.pudding_slice_nutrition;
     }
 
     public static float getPuddingSliceSaturation() {
-        return BakeryNeoForgeConfig.PUDDING_SLICE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.pudding_slice_saturation;
     }
 
+    // Cookies
     public static int getStrawberryGlazedCookieNutrition() {
-        return BakeryNeoForgeConfig.STRAWBERRY_GLAZED_COOKIE_NUTRITION.get();
+        return BakeryNeoForgeConfig.strawberry_glazed_cookie_nutrition;
     }
 
     public static float getStrawberryGlazedCookieSaturation() {
-        return BakeryNeoForgeConfig.STRAWBERRY_GLAZED_COOKIE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.strawberry_glazed_cookie_saturation;
     }
 
     public static int getSweetberryGlazedCookieNutrition() {
-        return BakeryNeoForgeConfig.SWEETBERRY_GLAZED_COOKIE_NUTRITION.get();
+        return BakeryNeoForgeConfig.sweetberry_glazed_cookie_nutrition;
     }
 
     public static float getSweetberryGlazedCookieSaturation() {
-        return BakeryNeoForgeConfig.SWEETBERRY_GLAZED_COOKIE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.sweetberry_glazed_cookie_saturation;
     }
 
     public static int getChocolateGlazedCookieNutrition() {
-        return BakeryNeoForgeConfig.CHOCOLATE_GLAZED_COOKIE_NUTRITION.get();
+        return BakeryNeoForgeConfig.chocolate_glazed_cookie_nutrition;
     }
 
     public static float getChocolateGlazedCookieSaturation() {
-        return BakeryNeoForgeConfig.CHOCOLATE_GLAZED_COOKIE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.chocolate_glazed_cookie_saturation;
     }
 
+    // Cupcakes
     public static int getStrawberryCupcakeNutrition() {
-        return BakeryNeoForgeConfig.STRAWBERRY_CUPCAKE_NUTRITION.get();
+        return BakeryNeoForgeConfig.strawberry_cupcake_nutrition;
     }
 
     public static float getStrawberryCupcakeSaturation() {
-        return BakeryNeoForgeConfig.STRAWBERRY_CUPCAKE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.strawberry_cupcake_saturation;
     }
 
     public static int getSweetberryCupcakeNutrition() {
-        return BakeryNeoForgeConfig.SWEETBERRY_CUPCAKE_NUTRITION.get();
+        return BakeryNeoForgeConfig.sweetberry_cupcake_nutrition;
     }
 
     public static float getSweetberryCupcakeSaturation() {
-        return BakeryNeoForgeConfig.SWEETBERRY_CUPCAKE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.sweetberry_cupcake_saturation;
     }
 
     public static int getAppleCupcakeNutrition() {
-        return BakeryNeoForgeConfig.APPLE_CUPCAKE_NUTRITION.get();
+        return BakeryNeoForgeConfig.apple_cupcake_nutrition;
     }
 
     public static float getAppleCupcakeSaturation() {
-        return BakeryNeoForgeConfig.APPLE_CUPCAKE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.apple_cupcake_saturation;
     }
 
+    // Pastries & Others
     public static int getCornetNutrition() {
-        return BakeryNeoForgeConfig.CORNET_NUTRITION.get();
+        return BakeryNeoForgeConfig.cornet_nutrition;
     }
 
     public static float getCornetSaturation() {
-        return BakeryNeoForgeConfig.CORNET_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.cornet_saturation;
     }
 
     public static int getJamRollNutrition() {
-        return BakeryNeoForgeConfig.JAM_ROLL_NUTRITION.get();
+        return BakeryNeoForgeConfig.jam_roll_nutrition;
     }
 
     public static float getJamRollSaturation() {
-        return BakeryNeoForgeConfig.JAM_ROLL_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.jam_roll_saturation;
     }
 
     public static int getBunNutrition() {
-        return BakeryNeoForgeConfig.BUN_NUTRITION.get();
+        return BakeryNeoForgeConfig.bun_nutrition;
     }
 
     public static float getBunSaturation() {
-        return BakeryNeoForgeConfig.BUN_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.bun_saturation;
     }
 
     public static int getWaffleNutrition() {
-        return BakeryNeoForgeConfig.WAFFLE_NUTRITION.get();
+        return BakeryNeoForgeConfig.waffle_nutrition;
     }
 
     public static float getWaffleSaturation() {
-        return BakeryNeoForgeConfig.WAFFLE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.waffle_saturation;
     }
 
     public static int getChocolateTruffleNutrition() {
-        return BakeryNeoForgeConfig.CHOCOLATE_TRUFFLE_NUTRITION.get();
+        return BakeryNeoForgeConfig.chocolate_truffle_nutrition;
     }
 
     public static float getChocolateTruffleSaturation() {
-        return BakeryNeoForgeConfig.CHOCOLATE_TRUFFLE_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.chocolate_truffle_saturation;
     }
 
     public static int getMisslilituBiscuitNutrition() {
-        return BakeryNeoForgeConfig.MISSLILITU_BISCUIT_NUTRITION.get();
+        return BakeryNeoForgeConfig.misslilitu_biscuit_nutrition;
     }
 
     public static float getMisslilituBiscuitSaturation() {
-        return BakeryNeoForgeConfig.MISSLILITU_BISCUIT_SATURATION.get().floatValue();
+        return (float) BakeryNeoForgeConfig.misslilitu_biscuit_saturation;
     }
 }
